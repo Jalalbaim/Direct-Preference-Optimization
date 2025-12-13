43cfb2e9c04385eb06be51a523dce537d24b673b
Pierre A <pierre.anzala@ensta-paris.fr>
2025-12-13 17:02:54 +0100

    Test

--- scripts/summarization/prepare_summary_data.py
+++ scripts/summarization/prepare_summary_data.py
@@ -1,61 +1,49 @@
+#python3 scripts/summarization/prepare_summary_data.py
+
 #IMPORT DATA -----------------
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, AutoModelForCausalLM, AdamW
-
-
-ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(ROOT)
-
-from src.dpo.models import load_models, compute_logprobs
-from src.dpo.utils import load_yaml_config
-
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
+import sys
 from datasets import load_dataset
 
-#LOAD DATA -----------------
-ds = load_dataset("openai/summarize_from_feedback", "comparisons")
+ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT)
+
+from src.dpo.utils import load_yaml_config
 
 #MAIN FUNCTIONS -----------------
-def load_summary_classifier(model_name: str, device: str):
-    clf_tokenizer = AutoTokenizer.from_pretrained(model_name)
-    clf_model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        use_safetensors=True,  
+def format_dpo(dataset):
+    return dataset.map(
+        lambda x: {
+            "prompt": x["info"]["post"].strip(),
+            "chosen": x["summaries"][x["choice"]]["text"].strip(),
+            "rejected": x["summaries"][1 - x["choice"]]["text"].strip(),
+        },
+        remove_columns=dataset.column_names,
     )
-    clf_model.to(device)
-    clf_model.eval()
-    return clf_tokenizer, clf_model
 
 
-def format_dpo(ds):
-    return ds.map(lambda x: {
-        "prompt": x["info"]["post"],
-        "chosen": x["summaries"]["chosen"],
-        "rejected": x["summaries"]["rejected"],
-    })
+def main():
+    config_path = "configs/summary.yaml"
+    config = load_yaml_config(config_path)
 
 
-@torch.no_grad()
-def summary_score(texts, clf_tokenizer, clf_model, device: str):
+    #LOAD DATASET WITH HUMAN PREFERENCES -----------------
+    ds = load_dataset("openai/summarize_from_feedback", "comparisons")
 
-    text_final = texts.prompt + "\n\nSummary:" + texts.answer
+    #FORMAT DATASET -----------------
+    ds = format_dpo(ds["train"].select(range(config["data"]["prompt_nb"])))
 
-    enc = clf_tokenizer(
-        text_final,
-        padding=True,
-        truncation=True,
-        max_length=512,
-        return_tensors="pt",
-    ).to(device)
+    #SAVE DATASET -----------------
+    os.makedirs("data/processed/summarization", exist_ok=True)
+    ds.to_json("data/processed/summarization/summarization_dpo_data.jsonl")
 
-    outputs = clf_model(**enc)
-    logits = outputs.logits
+if __name__ == "__main__":
+    main()
 
-    #Transformer en proba avec le softmax
-    log_probs = torch.log_softmax(logits, dim=-1)
-    last_token_log_probs = log_probs[:, -1, :]
-    scores = torch.max(last_token_log_probs, dim=-1).values  
 
-    return scores.cpu()
 
+    
