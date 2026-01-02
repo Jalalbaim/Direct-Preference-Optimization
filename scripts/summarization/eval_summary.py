26d62682b24693d31122fa62981d123dfcdc15de
Pierre A <pierre.anzala@ensta-paris.fr>
2026-01-02 19:54:14 +0100

    Change

--- scripts/summarization/eval_summary.py
+++ scripts/summarization/eval_summary.py
@@ -172,7 +172,7 @@ def main():
     test_ds = dataset["test"].select(range(10))
 
     # Chat pipeline
-    model_name = "EleutherAI/gpt-j-6B"
+    model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
